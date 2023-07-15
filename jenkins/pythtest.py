e685c9e9b46be0988fbe1fb17a79e77c0e87ba15
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2023-07-15 14:51:39 -0700

    Update pythtest.py

--- jenkins/pythtest.py
+++ jenkins/pythtest.py
@@ -1,4 +1,4 @@
-import sys
+"""import sys
 def callFunc(a, b):
   a = "why"
   b = "Aditi"
@@ -8,4 +8,20 @@ if __name__ == '__main__':
     # args[0] = current file
     # args[1] = function name
     # args[2:] = function args : (*unpacked)
-  globals()[sys.argv[1]](sys.argv[2])
+  globals()[callFunc](*args[2:])
+"""
+
+import sys
+
+def print_fn():
+    print("Hi")
+
+def sum_fn(a, b):
+    print(a + b)
+
+if __name__ == "__main__":
+    args = sys.argv
+    # args[0] = current file
+    # args[1] = function name
+    # args[2:] = function args : (*unpacked)
+    globals()[args[1]](*args[2:])
